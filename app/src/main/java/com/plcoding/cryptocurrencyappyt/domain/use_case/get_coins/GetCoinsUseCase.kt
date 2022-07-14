4da0e11e8ce9fd409d8050799064ffbf52b8136a
Gelson Nascimento <gelsonschikorski@hotmail.com>
2022-07-14 10:39:37 -0300

    Adiciona classes restantes e permissão de internet. Curso Finalizado.

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -16,13 +16,13 @@ class GetCoinsUseCase @Inject constructor(
     //overriding invoke u can call GetCoinsUseCase directly as if it was a function
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<List<Coin>>())
             val coins = repository.getCoins().map { it.toCoin() }
-            emit(Resource.Success(coins))
+            emit(Resource.Success<List<Coin>>(coins))
         } catch (e: HttpException) {
-            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured"))
         } catch (e: IOException) {
-            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet connection"))
         }
     }
 }
